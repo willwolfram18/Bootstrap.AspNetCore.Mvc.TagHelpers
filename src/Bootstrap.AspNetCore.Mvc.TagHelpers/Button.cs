0ea2887400d92bf6b6c0ef45cae1ee4e001d3c75
TheWolfA2 <wolfington98@gmail.com>
2017-01-13 07:36:32 -0800

    Button finished

--- src/Bootstrap.AspNetCore.Mvc.TagHelpers/Button.cs
+++ src/Bootstrap.AspNetCore.Mvc.TagHelpers/Button.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Bootstrap.AspNetCore.Mvc.TagHelpers
@@ -30,9 +31,11 @@ namespace Bootstrap.AspNetCore.Mvc.TagHelpers
     {
         #region Properties
         #region Public properties
-        public const string REQUIRED_ATTRIBUTE_NAMES = BUTTON_TYPE_ATTRIBUTE_NAME + "," + OUTPUT_TAG_ATTRIBUTE_NAME;
+        public const string REQUIRED_ATTRIBUTE_NAMES = OUTPUT_TAG_ATTRIBUTE_NAME + "," + BUTTON_TYPE_ATTRIBUTE_NAME;
         public const string BUTTON_TYPE_ATTRIBUTE_NAME = "btn-type";
         public const string BUTTON_SIZE_ATTRIBUTE_NAME = "btn-size";
+        public const string BUTTON_ACTIVE_ATTRIBUTE_NAME = "btn-active";
+        public const string BUTTON_DISABLED_ATTRIBUTE_NAME = "btn-disabled";
 
         [HtmlAttributeName(BUTTON_TYPE_ATTRIBUTE_NAME)]
         public ButtonType ButtonType { get; set; }
@@ -40,23 +43,41 @@ namespace Bootstrap.AspNetCore.Mvc.TagHelpers
         [HtmlAttributeName(BUTTON_SIZE_ATTRIBUTE_NAME)]
         public ButtonSize ButtonSize { get; set; } = ButtonSize.normal;
 
+        [HtmlAttributeName(BUTTON_ACTIVE_ATTRIBUTE_NAME)]
+        public bool IsActive { get; set; } = false;
+
+        [HtmlAttributeName(BUTTON_DISABLED_ATTRIBUTE_NAME)]
+        public bool IsDisabled { get; set; } = false;
+
         public override string CssClass
         {
             get
             {
-                string cssClass = $"btn btn-{ButtonType.ToString().ToLower()}";
+                StringBuilder cssClass = new StringBuilder($"btn btn-{ButtonType.ToString().ToLower()}");
                 switch (ButtonSize)
                 {
                     case ButtonSize.xs:
                     case ButtonSize.sm:
                     case ButtonSize.lg:
-                        cssClass += $" btn-{ButtonSize}";
+                        cssClass.Append($" btn-{ButtonSize}");
                         break;
                     case ButtonSize.normal:
                     default:
                         break;
                 }
-                return cssClass;
+
+                if (IsActive)
+                {
+                    cssClass.Append(" active");
+                }
+                if (IsDisabled)
+                {
+                    if (OutputTag.Trim() != "button")
+                    {
+                        cssClass.Append(" disabled");
+                    }
+                }
+                return cssClass.ToString();
             }
         }
         #endregion
@@ -67,6 +88,14 @@ namespace Bootstrap.AspNetCore.Mvc.TagHelpers
             var content = await output.GetChildContentAsync();
             output.Content.AppendHtml(content);
             output.TagName = OutputTag;
+            if (IsActive)
+            {
+                output.Attributes.SetAttribute("aria-pressed", "true");
+            }
+            if (IsDisabled && OutputTag.Trim() == "button")
+            {
+                output.Attributes.SetAttribute("disabled", "disabled");
+            }
             AppendDefaultCssClass(output);
         }
     }
