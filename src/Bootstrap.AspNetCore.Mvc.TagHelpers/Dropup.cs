69746c129542eeec7cfc36e36a1c48983a0e41a2
TheWolfA2 <wolfington98@gmail.com>
2017-01-18 07:35:35 -0800

    Dropup support

---
+++ src/Bootstrap.AspNetCore.Mvc.TagHelpers/Dropup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootstrap.AspNetCore.Mvc.TagHelpers
+{
+    [HtmlTargetElement(TAG)]
+    public class Dropup : Dropdown
+    {
+        public const string TAG = Global.PREFIX + "dropup";
+
+        public override string CssClass
+        {
+            get
+            {
+                return "dropup";
+            }
+        }
+    }
+
+    [HtmlTargetElement(TAG, ParentTag = Dropup.TAG)]
+    public class DropupItem : DropdownItem
+    {
+    }
+}
