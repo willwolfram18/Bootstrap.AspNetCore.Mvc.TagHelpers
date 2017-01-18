b1a1d04c3daa5471c39acde0291783b0a9ad7ec4
TheWolfA2 <wolfington98@gmail.com>
2017-01-18 07:00:14 -0800

    Continued dropdown tag

--- src/Bootstrap.AspNetCore.Mvc.TagHelpers/Dropdown.cs
+++ src/Bootstrap.AspNetCore.Mvc.TagHelpers/Dropdown.cs
@@ -23,7 +23,7 @@ namespace Bootstrap.AspNetCore.Mvc.TagHelpers
         #region Properties
         #region Public Properties
         public const string TAG = Global.PREFIX + "dropdown";
-        public const string TEXT_ATTRIBUTE_NAME = "title";
+        public const string TEXT_ATTRIBUTE_NAME = "dropdown-text";
 
         public override string CssClass
         {
@@ -44,7 +44,94 @@ namespace Bootstrap.AspNetCore.Mvc.TagHelpers
 
         #region Methods
         #region Public Methods
-        
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await base.ProcessAsync(context, output);
+            context.Items.Add(_dropdownContext.GetType(), _dropdownContext);
+
+            var childContent = await output.GetChildContentAsync();
+        }
+        #endregion
+        #endregion
+    }
+
+    public enum DropdownItemType
+    {
+        normal,
+        header,
+        divider,
+    }
+
+    [HtmlTargetElement(TAG, ParentTag = Dropdown.TAG)]
+    public class DropdownItem : BootstrapTagHelperBase
+    {
+        #region Properties
+        #region Public properties
+        public const string TAG = Global.PREFIX + "dropdown-item";
+        public const string ACTIVE_ATTRIBUTE_NAME = "dropdown-item-active";
+        public const string DISABLED_ATTRIBUTE_NAME = "dropdown-item-disabled";
+        public const string TYPE_ATTRIBUTE_NAME = "dropdown-item-type";
+
+        public override string CssClass
+        {
+            get
+            {
+                if (ItemType == DropdownItemType.divider)
+                {
+                    return "divider";
+                }
+                if (ItemType == DropdownItemType.header)
+                {
+                    return "dropdown-header";
+                }
+
+                List<string> cssClasses = new List<string>();
+                if (IsActive)
+                {
+                    cssClasses.Add("active");
+                }
+                if (IsDisabled)
+                {
+                    cssClasses.Add("disabled");
+                }
+                return string.Join(" ", cssClasses);
+            }
+        }
+
+        [HtmlAttributeName(ACTIVE_ATTRIBUTE_NAME)]
+        public bool IsActive { get; set; } = false;
+
+        [HtmlAttributeName(DISABLED_ATTRIBUTE_NAME)]
+        public bool IsDisabled { get; set; } = false;
+
+        [HtmlAttributeName(TYPE_ATTRIBUTE_NAME)]
+        public DropdownItemType ItemType { get; set; } = DropdownItemType.normal;
+
+        [HtmlAttributeNotBound]
+        public override string OutputTag { get; set; } = "li";
+        #endregion
+        #endregion
+
+        #region Methods
+        #region Public methods
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await base.ProcessAsync(context, output);
+
+            DropdownContext dropdownContext = context.Items[typeof(DropdownContext)] as DropdownContext;
+            if (ItemType == DropdownItemType.divider)
+            {
+                output.Attributes.SetAttribute("role", "separator");
+                output.Content.SetHtmlContent("");
+            }
+            else
+            {
+                var childContent = await output.GetChildContentAsync();
+                output.Content.SetHtmlContent(childContent);
+            }
+            dropdownContext.DropdownItems.Add(output);
+            output.SuppressOutput();
+        }
         #endregion
         #endregion
     }
