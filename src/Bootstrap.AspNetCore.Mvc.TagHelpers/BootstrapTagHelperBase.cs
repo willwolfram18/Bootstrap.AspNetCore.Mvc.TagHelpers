29b7d2ff8f730fe24752dadad59e7ff465d88ea8
TheWolfA2 <wolfington98@gmail.com>
2017-01-19 07:56:42 -0800

    Licensing added

--- src/Bootstrap.AspNetCore.Mvc.TagHelpers/BootstrapTagHelperBase.cs
+++ src/Bootstrap.AspNetCore.Mvc.TagHelpers/BootstrapTagHelperBase.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2016 Billy Wolfington
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ * https://github.com/Bwolfing/Bootstrap.AspNetCore.Mvc.TagHelpers
+ *
+ */
+
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
--- src/Bootstrap.AspNetCore.Mvc.TagHelpers/ButtonGroup.cs
+++ src/Bootstrap.AspNetCore.Mvc.TagHelpers/ButtonGroup.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.AspNetCore.Html;
+﻿/*
+ * Copyright (c) 2016 Billy Wolfington
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ * https://github.com/Bwolfing/Bootstrap.AspNetCore.Mvc.TagHelpers
+ *
+ */
+
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
